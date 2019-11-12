bd83b2215b69554768e4556a9a0e3d036e44510f
Chika <catherine.achukwu@gmail.com>
2019-11-12 16:52:26 -0500

    app.js console log working

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 const events = require("./index.js")
-const listEvent = events()
+const fs = require('fs');
+const readline = require('readline');
+const { google } = require('googleapis');
+const authorize = events.authorize
+const listEvents = events.listEvents
+
 console.log("here we are") 
+// Load client secrets from a local file.
+fs.readFile('credentials.json', (err, content) => {
+    if (err) return console.log('Error loading client secret file:', err);
+    // Authorize a client with credentials, then call the Google Calendar API.
+    authorize(JSON.parse(content), listEvents);
+    
+});
